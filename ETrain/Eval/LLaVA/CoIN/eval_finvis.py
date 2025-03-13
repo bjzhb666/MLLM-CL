5708d02919dbf44a9f6500aae0a962be0a164f0c
bjzhb666 <bjzhb666@outlook.com>
2025-03-13 15:57:30 +0800

    update eval_finvis

--- ETrain/Eval/LLaVA/CoIN/eval_finvis.py
+++ ETrain/Eval/LLaVA/CoIN/eval_finvis.py
@@ -2,8 +2,6 @@ import os
 import argparse
 import json
 import re
-from evaluate import load
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -13,43 +11,48 @@ def get_args():
     return parser.parse_args()
 
 def eval_single(annotation_file, result_file):
-    bertscore = load("bertscore", module_type="metric", cache_dir="metrics")
     experiment_name = os.path.splitext(os.path.basename(result_file))[0]
     annotations = json.load(open(annotation_file))
     annotations = {data['question_id']: data for data in annotations}
     results = [json.loads(line) for line in open(result_file)]
 
-    pred_list = []
     total = len(results)
     right = 0
+    pred_list = []
     for result in results:
         annotation = annotations[result['question_id']]
         ground_truth = annotation['answer']
-        score = bertscore.compute(predictions=[result['text']], references=[ground_truth], lang='zh')['f1']
-        right += score[0]
+        problem = result['prompt']
+        image = annotation['image']
+        if 'Unanswerable' in result['text'] :
+            continue
+        
+        pred: str = result['text'].lower()
+        gt: str =  ground_truth.lower()
+        if gt == pred: # all are multiple choice questions or TF questions
+            right += 1
+        # save the result as jsonl
         pred_list.append(dict(
-            question=annotation['text'],
-            prediction=result['text'],
-            ground_truth=ground_truth,
-            score=score[0],
+            question=problem,
+            pred=result['text'].lower(),
+            ground_truth=ground_truth.lower(),
+            image=image,
+            score=1 if pred == gt else 0,
         ))
-        # if 'Unanswerable' in result['text'] :
-        #     continue
-        # if result['text'].lower() == ground_truth.lower():
-        #     right += 1
-
-    print('Samples: {}\nAccuracy: {:.2f}%\n'.format(len(pred_list), 100. * right / total))
+    print('Samples: {}\nAccuracy: {:.2f}%\n'.format(total, 100. * right / total))
     #将结果写入文件
     if args.output_dir is not None:
         output_file = os.path.join(args.output_dir, 'Result.text')
         with open(output_file, 'w') as f:
-            f.write('Samples: {}\nAccuracy: {:.2f}%\n'.format(len(pred_list), 100. * right / total))
+            f.write('Samples: {}\nAccuracy: {:.2f}%\n'.format(total, 100. * right / total))
+        # 将pred_list结果写入jsonl文件
         output_file = os.path.join(args.output_dir, 'Result.json')
         with open(output_file, 'w') as f:
-            json.dump(pred_list, f)
+            for item in pred_list:
+                json.dump(item, f)
+                f.write('\n')        
 
 if __name__ == "__main__":
-    # list_evaluation_modules()
     args = get_args()
 
     if args.result_file is not None:
