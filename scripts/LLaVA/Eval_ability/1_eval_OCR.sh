b394e9f6cbf7fda6b04e642af79a6e5d7fd5c271
bjzhb666 <bjzhb666@outlook.com>
2025-03-23 11:36:51 +0800

    add ocr test

---
+++ scripts/LLaVA/Eval_ability/1_eval_OCR.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0,1,2,3,4,5,6,7}" # 
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+if [ ! -n "$1" ] ;then
+    STAGE='Finetune'
+else
+    STAGE=$1
+fi
+
+if [ ! -n "$2" ] ;then
+    MODELPATH='./checkpoints/LLaVA/Ability/OCR_llava_lora'
+else
+    MODELPATH=$2
+fi
+if [ ! -n "$3" ] ;then
+    MODELNAME='OCR_ocr'
+else
+    MODELNAME=$3
+fi
+
+if [ ! -n "$4" ] ;then
+    MODELBASE='LLaVA'
+else
+    MODELBASE=$4
+fi
+
+RESULT_DIR="./results/CoIN/$MODELBASE/$MODELNAME"
+# RESULT_DIR="./results/CoIN/LLaVA/OCRVQA"
+DATA_PATH=/data/hongbo_zhao/Ability_data
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m ETrain.Eval.LLaVA.CoIN.model_ocr \
+        --model-path $MODELPATH \
+        --model-base ./checkpoints/LLaVA/Vicuna/vicuna-7b-v1.5 \
+        --question-file $DATA_PATH/OCR_test/test.json \
+        --image-folder $DATA_PATH/OCR_test \
+        --answers-file $RESULT_DIR/$STAGE/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 &
+done
+
+wait
+
+output_file=$RESULT_DIR/$STAGE/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $RESULT_DIR/$STAGE/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python -m ETrain.Eval.LLaVA.CoIN.eval_ocr \
+    --annotation-file $DATA_PATH/OCR_test/test.json \
+    --result-file $output_file \
+    --output-dir $RESULT_DIR/$STAGE \
