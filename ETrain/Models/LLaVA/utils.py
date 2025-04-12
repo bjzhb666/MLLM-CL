24816b6e85b48edf036e76d768aaffe19fd9a4f0
bjzhb666 <bjzhb666@outlook.com>
2025-04-12 17:19:01 +0800

    update code for visual lora

--- ETrain/Models/LLaVA/utils.py
+++ ETrain/Models/LLaVA/utils.py
@@ -65,7 +65,8 @@ def find_all_linear_names(model):
             continue
         if isinstance(module, cls):
             names = name.split('.')
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            # lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            lora_module_names.add(name)
 
     if 'lm_head' in lora_module_names: # needed for 16-bit
         lora_module_names.remove('lm_head')
